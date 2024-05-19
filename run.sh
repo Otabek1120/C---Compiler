9ddb9df50f6f9263f9bf143114f9af41f8273e15
Otabek <otabekabduraimov11@gmail.com>
2024-05-19 14:18:20 -0700

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 make clean
 make compile
 
-# ./compile --chk_decl --gen_code --print_ast <test1.txt >testout.txt
-./compile --chk_decl --gen_code  --print_ast <test1.txt >&testout.txt
+# ./compile --chk_decl --gen_code  --print_ast <test1.txt >&testout.txt
+./compile --chk_decl --print_ast <test1.txt >&testout.txt
+
+
 make clean
